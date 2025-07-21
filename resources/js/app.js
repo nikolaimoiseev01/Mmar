3849a0d80f51d112c57743f5b785fc42e5258a6a
nikolaimoiseev01 <89159295+nikolaimoiseev01@users.noreply.github.com>
2025-07-21 09:52:10 +0800

    bugs

--- resources/js/app.js
+++ resources/js/app.js
@@ -157,32 +157,32 @@ gsap.utils.toArray('.smooth-content').forEach((el) => {
 
 ;
 if (window.APP_ENV === 'production') {
-    document.body.style.setProperty('cursor', 'none', 'important');
-
-    document.querySelectorAll('*').forEach(el => {
-        el.style.setProperty('cursor', 'none', 'important');
-    });
-    const cursor = document.getElementById('cursor')
-
-    let mouseX = 0
-    let mouseY = 0
-    let currentX = 0
-    let currentY = 0
-
-    document.addEventListener('mousemove', (e) => {
-        mouseX = e.clientX
-        mouseY = e.clientY
-    })
-
-    function animateCursor() {
-        currentX += (mouseX - currentX) * 0.2
-        currentY += (mouseY - currentY) * 0.2
-
-        cursor.style.transform = `translate(${currentX}px, ${currentY}px)`
-        requestAnimationFrame(animateCursor)
-    }
-
-    animateCursor()
+    // document.body.style.setProperty('cursor', 'none', 'important');
+    //
+    // document.querySelectorAll('*').forEach(el => {
+    //     el.style.setProperty('cursor', 'none', 'important');
+    // });
+    // const cursor = document.getElementById('cursor')
+    //
+    // let mouseX = 0
+    // let mouseY = 0
+    // let currentX = 0
+    // let currentY = 0
+    //
+    // document.addEventListener('mousemove', (e) => {
+    //     mouseX = e.clientX
+    //     mouseY = e.clientY
+    // })
+    //
+    // function animateCursor() {
+    //     currentX += (mouseX - currentX) * 0.2
+    //     currentY += (mouseY - currentY) * 0.2
+    //
+    //     cursor.style.transform = `translate(${currentX}px, ${currentY}px)`
+    //     requestAnimationFrame(animateCursor)
+    // }
+    //
+    // animateCursor()
 }
 
 
