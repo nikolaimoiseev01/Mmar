95ea957d895ddf35f354dd45460818c50ad292f6
Nikolai <nikolaimoiseev01@gmail.com>
2025-08-26 17:34:40 +0300

    First Commit

--- resources/views/components/logo/logo-text-dark.blade.php
+++ resources/views/components/logo/logo-text-dark.blade.php
@@ -1,5 +1,5 @@
 <a href="/" wire:navigate class="w-fit">
-    <img src="/fixed/logo_text_dark.svg"
+    <img src="/fixed/logo_text_dark.png"
          {{ $attributes->merge(['class' => '']) }}
          alt="logo">
 </a>
