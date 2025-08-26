b21c79ab274349480fabf2a0223b03f6ffca913a
Nikolai <nikolaimoiseev01@gmail.com>
2025-08-26 17:03:20 +0300

    First Commit

--- resources/views/components/logo/logo-text.blade.php
+++ resources/views/components/logo/logo-text.blade.php
@@ -1,5 +1,5 @@
 <a href="/" wire:navigate class="w-fit">
-    <img src="/fixed/logo_text_light.svg"
+    <img src="/fixed/logo_text_light.png"
          {{ $attributes->merge(['class' => '']) }}
          alt="logo">
 </a>
